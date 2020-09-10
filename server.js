004dff07ae514011440a76a71603195028184c60
jeanhern81 <jeanhern81@gmail.com>
2020-09-10 16:22:18 -0700

    changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
