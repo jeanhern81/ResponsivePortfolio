fe3dbe0910bacb2641a83603f7f78fd1b97d8333
jeanhern81 <jeanhern81@gmail.com>
2020-09-11 13:43:58 -0700

    path fix

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 module.exports = function(app){ 
     app.get("/",function (req,res){   
-        res.sendFile(path.join(__dirname,"../public/index.html"));
+        res.sendFile(path.join(__dirname, "/public/index.html"));
     });
 
     app.get("/contact",function(req,res){    
-        res.sendFile(path.join(__dirname,"../public/contact.html"));
+        res.sendFile(path.join(__dirname, "/public/contact.html"));
     });
 
     app.get("/portfolio",function(req,res){   
-        res.sendFile(path.join(__dirname,"../public/portfolio.html"));
+        res.sendFile(path.join(__dirname, "/public/portfolio.html"));
     });
 
 }
