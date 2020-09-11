28778c400b53fd4ea5cdc47ccd8951dba310103a
jeanhern81 <jeanhern81@gmail.com>
2020-09-11 15:46:19 -0700

    trying to fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 //routes
 
