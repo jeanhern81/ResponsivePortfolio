65088e4efeb537a7af405ffb5130d9199faf8904
jeanhern81 <jeanhern81@gmail.com>
2020-09-11 14:05:59 -0700

    path

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //routes
+app.get('/', (req, res) => {
+    var options = {
+        root: path.join(__dirname, './public/index.html')
+    }
+    
+});
 /*
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
