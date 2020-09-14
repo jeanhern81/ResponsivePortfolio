701a2f845675a3ec1cd86cb2e529f8273fa6b15f
jeanhern81 <jeanhern81@gmail.com>
2020-09-14 13:49:01 -0700

    redid server file

---
+++ mail.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+require('dotenv').config()
+
+//console.log(process.env);
+
+var api_key = process.env.varapi_key;
+var domain = process.env.vardomain;
+
+
+const auth = {
+    auth: {
+            "api_key": process.env.api_key,
+            "domain": process.env.domain
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+    from: email,
+    to: 'jeanette@thewebdevstudio.com',
+    subject,
+    text
+    };
+
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            cb(err, null);
+        }   else {
+            cb(null, data);
+        }
+    });
+}
+
+
+module.exports = sendMail;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const sendMail = ('./mail');
+const app = express();
+const path = require('path');
+
+
+const PORT = 8080;
+
+app.use(express.urlencoded ({
+    extended: false
+}));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+
+app.post('/email', function(req, res) {
+    //TODO:
+    //send email here
+    const { subject, email, text } = req.body;
+    console.log('Data: ', req.body);
+
+    sendMail(email, subject, text, function(err, data){
+        if(err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            res.status({ message: 'Email sent!' });
+        }
+    });
+
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`App is listening on port ${PORT}!`);
+});
