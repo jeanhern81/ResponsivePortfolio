27c707cf56e0c30c59e686b37ef8b9004a5f1ba4
jeanhern81 <jeanhern81@gmail.com>
2020-09-14 14:03:42 -0700

    fixing path'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded ({
     extended: false
 }));
 app.use(express.json());
-
+app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
